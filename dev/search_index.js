bd9931c786931e937641c67fbab0075b85929b4b
Documenter.jl <documenter@juliadocs.github.io>
2023-03-01 20:15:47 +0000

    build based on 26e4a8e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#LocalHilbertBurch.jl","page":"LocalHilbertBurch.jl","title":"LocalHilbertBurch.jl","text":"","category":"section"},{"location":"","page":"LocalHilbertBurch.jl","title":"LocalHilbertBurch.jl","text":"Documentation for LocalHilbertBurch.jl","category":"page"}]
+}
